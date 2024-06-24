7c408ccc9edd97e097fbf875252911c80d1ca0d2
Oleksandr1112 <pakholchako@gmail.com>
2024-06-23 23:11:03 -0400

    bug fixing finished

--- src/components/ConnectCCP.jsx
+++ src/components/ConnectCCP.jsx
@@ -8,10 +8,10 @@ import HangUpButton from "./phone/HandUpButton.tsx";
 
 const ConnectCCP = ({ phoneNum }) => {
   const ref = useRef();
-
+  const number = phoneNum.replace(/\D/g, "");
   useEffect(() => {
     try {
-      connect.core.initCCP(document.getElementById("ccp"), {
+      connect.core.initCCP(ref.current, {
         ccpUrl: "https://tbi-test-connect.my.connect.aws/connect/ccp-v2",
         region: "us-east-1",
         loginPopup: true, // optional, defaults to `true`
@@ -39,52 +39,104 @@ const ConnectCCP = ({ phoneNum }) => {
         ccpSynTimeout: 3000, //optional, defaults to 1000 (ms)
         ccpLoadTimeout: 10000, //optional, defaults to 5000 (ms)
       });
-    } catch (error) {}
+      global.connect.agent((agent) => {
+        console.log("Agent initialized");
+
+        // Handle agent state changes
+        agent.onStateChange((state) => {
+          console.log("Agent state changed:", state);
+        });
+
+        // Handle agent error
+        agent.onError((error) => {
+          console.error("Agent error:", error);
+        });
+
+        // Handle agent availability
+        agent.onRoutableState((routableState) => {
+          console.log("Agent routable state:", routableState);
+        });
+      });
+      global.connect.contact(subscribeToContactEvents);
+      console.log("initCCP end");
+    } catch (error) {
+      console.error("Error initializing CCP:", error);
+    }
 
     return () => {
       // Cleanup: Disconnect from Amazon Connect when component unmounts
-      try {
-        connect.core.disconnect();
-      } catch (error) {}
     };
   }, [ref]);
 
-  const disconnectHandler = () => {
-    try {
-      connect.core.disconnect();
-    } catch (error) {}
+  var c;
+  const subscribeToContactEvents = (contact) => {
+    c = contact;
+    console.log("Subscribing to events for contact");
+    contact.onConnected(handleContactConnected);
+    contact.onEnded(handleContactEnded);
   };
 
-  const acceptHandler = () => {
-    const countryCode = "1";
-    let filteredNum = countryCode + phoneNum.replace(/\D/g, "");
-    //Debugging only -remove this later
-    console.log("phone number : ", filteredNum);
+  const handleContactConnected = (contact) => {
+    if (contact) {
+      console.log(
+        "[contact.onConnected] Contact connected to agent. Contact state is " +
+          contact.getStatus().type
+      );
+    } else {
+      console.log(
+        "[contact.onConnected] Contact connected to agent. Null contact passed to event handler"
+      );
+    }
+  };
 
-    let callerID = "";
-    window.contact
-      ? (callerID = window.contact.getAttributes().callerID)
-      : (callerID = "695227e1-08a7-41ff-b42e-1fd6f882ea55");
+  const handleContactEnded = (contact) => {
+    if (contact) {
+      console.log(
+        "[contact.onEnded] Contact has ended. Contact state is " +
+          contact.getStatus().type
+      );
+      //contact.clear();
+    } else {
+      console.log(
+        "[contact.onEnded] Contact has ended. Null contact passed to event handler"
+      );
+    }
+  };
 
-    console.log(callerID);
-    axios
-      .get(
-        `https://o2xpogtamg.execute-api.us-east-1.amazonaws.com/dev/GetConnectManager?destPhone=%2B${filteredNum}&queueARN=${callerID}`
-      )
-      .then((response) => {
-        console.log("API call successful", response);
-      })
-      .catch((error) => {
-        console.error("API call error", error);
-      });
+  const disconnectCall = () => {
+    c.getAgentConnection().destroy({
+      success: function () {
+        console.log("Disconnected contact via Streams");
+      },
+      failure: function () {
+        console.log("Failed to disconnect contact via Streams");
+      },
+    });
+  };
+
+  const ouboundCall = () => {
+    var endpoint = connect.Endpoint.byPhoneNumber("+1" + number);
+    var agent = new connect.Agent();
+    //var queueArn = "arn:aws:connect:<REGION>:<ACCOUNT_ID>:instance/<CONNECT_INSTANCE_ID>/queue/<CONNECT_QUEUE_ID>";
+
+    agent.connect(endpoint, {
+      //queueARN: queueArn,
+      success: function () {
+        console.log("outbound call connected");
+      },
+      failure: function (err) {
+        console.log("outbound call connection failed");
+        console.log(err);
+      },
+    });
   };
 
   return (
     <>
-      <div id="ccp" ref={ref} style={{ display: "none" }} />
+      <div ref={ref} style={{ display: "none" }} />
       <div className="flex justify-between mb-5">
-        <CallButton acceptHandler={acceptHandler} />
-        <HangUpButton disconnectHandler={disconnectHandler} />
+        <CallButton acceptHandler={ouboundCall} />
+        <HangUpButton disconnectHandler={disconnectCall} />
       </div>
     </>
   );
